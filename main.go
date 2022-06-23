c93237395ddb46678422fc575d6b4dc24bc8a891
junginkim-catenoid <jungin.kim@catenoid.net>
2022-06-23 15:45:41 +0900

    gin framework apply

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 	println("Hello CallbackMediaWatcher app")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		//time.Sleep(time.Second * 15)
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":3002") //port
 }
